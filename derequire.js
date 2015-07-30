0d42b67706d0941740b23936158fc51e02d08174
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2015-07-30 15:11:56 +0900

    initial import

---
+++ derequire.js
@@ -0,0 +1,61 @@
+
+var path = require('path');
+var fs = require('fs');
+
+function getpath(module, object) {
+	for (var srcpath in module._cache) {
+		if (module._cache[srcpath].exports == object) {
+			return srcpath;
+		}
+	}
+	return null;
+}
+
+function isroot(dir) {
+	if (dir === "/") {
+		return true;
+	}
+
+	if (/[a-zA-Z]:\\$/.test(dir)) {
+		return true;
+	}
+
+	return false;
+}
+
+function checkmain(maindef, srcpath) {
+	if (maindef === srcpath) {
+		return true;
+	}
+	if (path.join(maindef, 'index.js') === srcpath) {
+		return true;
+	}
+
+}
+
+function searchpackage(modulepath) {
+	var parentdir = path.dirname(modulepath);
+
+	while (!isroot(parentdir)) {
+		try {
+			var packagejson = JSON.parse(fs.readFileSync(parentdir + "/package.json"));
+			if (packagejson && chackmain(packagejson.main, modulepath)) {
+				return packagejson;
+			}
+		} catch (e) {
+		}
+		parentdir = path.dirname(parentdir);
+	}
+	return null;
+}
+
+module.exports = function(object) {
+	var module = require('module');
+	var modulepath = getpath(module, object);
+	var result = { path: modulepath };
+	if (modulepath) {
+		result.packagejson = searchpackage(modulepath);
+	}
+	return result;
+}
+
